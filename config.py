9e2788176bd8a751f55148bc602f4c2923a856ac
eliblurr <elvissegbawu@gmail.com>
2021-09-16 00:24:52 +0000

    twilio sms trial

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ JWT_ALGORITHM = "HS256"
 class Settings(BaseSettings):
     DATABASE_URL: str
     ACCESS_SESSION_DURATION_IN_MINUTES: int = 30
-    TWILIO_PHONE_NUMBER: str = '+16196584362'
+    TWILIO_PHONE_NUMBER: str = '+16196584362' 
     TWILIO_AUTH_TOKEN: str = '7b6c506ee07337cc3d02536d5119c4b2'
     TWILIO_ACCOUNT_SID: str = 'AC959cbde01aced5669b0121ffea2df117'
     SECRET: str = "eUCcDNVG$rVB6wUK2TjJRWptEN56b3_wQwxRdSq&R76wE6#m8+-A3sm4sKtLdH3hkug@T&@Szg__KjgjhnhyDHFnt%Ru#y8SxW*m=_7f$2uF9CArFq7LtmQqBNuQdTa@KLt@*Y%M24Ry=eUd%R6QsXW3=Z-g!!Rvu#srJ5*#PhbVq@6pyx=R2Jr7VhsYT_QT^j7uLqkqX2%RPnr9SS5RCbt6d!wY&+FQC6=&f37U&f+8JdXf!QV%pB?7V?QSwvrj"
--- services/sms.py
+++ services/sms.py
@@ -3,10 +3,7 @@ from config import settings
 
 client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
 
-print('start')
 # change the "from_" number to your Twilio number and the "to" number
 # to the phone number you signed up for Twilio with, or upgrade your
 # account to send SMS to any phone number
 client.messages.create(to="+233543081518", from_=settings.TWILIO_PHONE_NUMBER, body="Hello from Python!")
-
-print('sent')
