b0d83baef66bfa4ca38bd0298d8a48ecbd98831c
eliblurr <elvissegbawu@gmail.com>
2021-09-24 12:52:26 +0000

    category debug

--- routers/category/models.py
+++ routers/category/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, Integer, ForeignKey, UniqueConstraint
+from sqlalchemy import Column, String, Integer, ForeignKey, UniqueConstraint, event
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import relationship
 from routers.media.models import Image
 from routers.meal.models import Meal
@@ -8,8 +9,7 @@ from database import Base
 class Category(BaseMixin, Base):
     '''Category Model'''
     __tablename__ = "categories"
-    __table_args__ = (UniqueConstraint('title', 'restaurant_id', name='uix_title_restaurant_fk'),)
-    # unique constraint on coalesce(restaurant_id not null & title) + title
+    __table_args__ = (UniqueConstraint('title', 'restaurant_id', name='uix_cat_title_restaurant_fk'),)
     
     title = Column(String, nullable=False)
     metatitle = Column(String, nullable=True)
@@ -24,3 +24,9 @@ class CategoryMeal(Base):
 
     meal_id = Column(Integer, ForeignKey('meals.id'), primary_key=True)
     category_id = Column(Integer, ForeignKey('categories.id'), primary_key=True)
+
+@event.listens_for(Category, 'before_insert')
+def verify_target_title(mapper, connection, target):
+    res = connection.execute(Category.__table__.select().where(Category.__table__.c.restaurant_id==None, Category.__table__.c.title==target.title))
+    if res.rowcount:  
+        raise IntegrityError(f"custom select @@ restaurant_id is NULL AND title={target.title}", 'title', f'DETAIL:  Key (title)=({target.title}) already exists.\n')  
