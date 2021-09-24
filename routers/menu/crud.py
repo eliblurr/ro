f39bc534e052829422618ea7d1ca984aa32c8b38
eliblurr <elvissegbawu@gmail.com>
2021-09-24 14:52:51 +0000

    menu meals debug

--- routers/menu/crud.py
+++ routers/menu/crud.py
@@ -1,4 +1,41 @@
+from utils import http_exception_detail
+from routers.meal.crud  import meal
+from fastapi import HTTPException
 from . import models, schemas
 from cls import CRUD
 
 menu = CRUD(models.Menu)
+
+async def add_meal_to_menu(resource_id, meal_ids, db):
+    mn = await menu.read_by_id(resource_id, db)
+    if not mn:
+        raise HTTPException(status_code=404, detail=http_exception_detail(loc='resource_id', msg=f'menu with id {resource_id} not found', type='NonExist'))
+    msg = []
+    try:
+        for id in meal_ids:
+            ml = await meal.read_by_id(id, db)
+            if ml and (ml not in mn.meals):
+                mn.meals.append(ml)
+            else:
+                msg.append(id)
+        db.commit()
+        return "success", {"loc":msg, "info":f"failed" if msg else "all meals added", "msg":"meal(s) already part of menu or no longer exists"}
+    except Exception as s:
+        raise HTTPException(status_code=500, detail=http_exception_detail(msg=f"{e}", type= e.__class__.__name__))
+
+async def remove_meal_from_menu(resource_id, meal_ids, db):
+    mn = await menu.read_by_id(resource_id, db)
+    if not mn:
+        raise HTTPException(status_code=404, detail=http_exception_detail(loc='resource_id', msg=f'menu with id {resource_id} not found', type='NonExist'))
+    msg = []
+    try:
+        for id in meal_ids:
+            ml = await meal.read_by_id(id, db)
+            if ml and (ml in mn.meals):
+                mn.meals.remove(ml)
+            else:
+                msg.append(id)
+        db.commit()
+        return "success", {"loc":msg, "info":f"failed" if msg else "all meals removed", "msg":"meal(s) is not part of menu or no longer exists"}
+    except Exception as s:
+        raise HTTPException(status_code=500, detail=http_exception_detail(msg=f"{e}", type= e.__class__.__name__))
--- routers/menu/main.py
+++ routers/menu/main.py
@@ -11,10 +11,6 @@ router = APIRouter()
 async def create(payload:schemas.CreateMenu=Depends(schemas.CreateMenu.as_form), images:List[UploadFile]=File(None), db:Session=Depends(get_db)):
     return await crud.menu.create(payload, db, images)
 
-# @router.post('/{resource_id}/meals', description='', response_model=schemas.Menu, name='Menu')
-# async def read_by_id(resource_id:int, db:Session=Depends(get_db)):
-#     return await crud.menu.read_by_id(resource_id, db)
-
 @router.get('/', description='', response_model=schemas.MenuList, name='Menu')
 @ContentQueryChecker(crud.menu.model.c(), None)
 async def read(db:Session=Depends(get_db), **params):
@@ -28,6 +24,14 @@ async def read_by_id(resource_id:int, db:Session=Depends(get_db)):
 async def update(resource_id:int, payload:schemas.UpdateMenu, db:Session=Depends(get_db)):
     return await crud.menu.update(resource_id, payload, db)
 
+@router.put('/{resource_id}/meals', description='', name='Menu')
+async def add_meal_to_menu(resource_id:int, meal_ids:List[int], db:Session=Depends(get_db)):
+    return await crud.add_meal_to_menu(resource_id, meal_ids, db)
+
 @router.delete('/{resource_id}', description='', name='Menu')
 async def delete(resource_id:int, db:Session=Depends(get_db)):
     return await crud.menu.delete(resource_id, db)
+
+@router.delete('/{resource_id}/meals', description='', name='Menu')
+async def add_meal_to_menu(resource_id:int, meal_ids:List[int], db:Session=Depends(get_db)):
+    return await crud.remove_meal_from_menu(resource_id, meal_ids, db)
